8c439aa459c054de2ec207d1d9e51bd92eff87d1
jaishnavis <jaishnavisaravanan@gmail.com>
2024-01-26 15:28:31 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import {BrowserRouter,Routes , Route} from "react-router-dom"
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import Contact from "./Pages/Contact"
-import Layout from "./Layout"
-import Service from "./Pages/Service"
+// import {BrowserRouter,Routes , Route} from "react-router-dom"
+// import Home from "./Pages/Home"
+// import About from "./Pages/About"
+// import Contact from "./Pages/Contact"
+// import Layout from "./Layout"
+// import Service from "./Pages/Service"
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Layout/>}>
+//       <Route index element={<Home/>} />
+//       <Route path="about" element={<About/>} />
+//       <Route path="contact"element={<Contact/>} />
+//       <Route path="service"element={<Service/>} />
+//       </Route>
+//     </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+import React from 'react'
+import Reducer from './Reducer'
+
 export default function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-      <Route index element={<Home/>} />
-      <Route path="about" element={<About/>} />
-      <Route path="contact"element={<Contact/>} />
-      <Route path="service"element={<Service/>} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <div>
+      <Reducer/>
+    </div>
   )
 }
